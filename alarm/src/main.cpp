f668bf3aa2c3836705a9b29bbfb605e3360a06a1
Rob James <97772344+robathanjames@users.noreply.github.com>
2023-10-26 16:34:50 -0700

    Update main.cpp
    a.)Error Handling: Added error checks for ESP-NOW initialization (esp_now_init()) and peer addition (esp_now_add_peer()). If an error occurs, a message will be printed to the serial monitor.
    
    b.)Use Constants for MAC Addresses: Changed static uint8_t to const uint8_t for MAC addresses to make it clear that these values should not be modified during runtime.
    
    c.)Modular Functions: Created separate functions init_esp_now() and add_peer() to handle ESP-NOW initialization and peer addition. This modularizes the code and makes it easier to manage.
    
    d.)Debounce for Button Input: Added a 50ms delay for simple debounce logic before reading the PARTY_STOP_PIN. Changed the pinMode to INPUT_PULLUP to avoid the need for an external pull-up resistor.

--- alarm/src/main.cpp
+++ alarm/src/main.cpp
@@ -5,54 +5,69 @@
 
 #define PARTY_STOP_PIN D2
 
-// this is a weak symbol in esp8266ArduinoCore code, override it so we save time by not disabling WiFi
-extern "C" void __disableWiFiAtBootTime(void) {}
+const uint8_t disco_mac[6] = {0xa4,0xe5,0x7c,0xbc,0xdb,0xe9};  // Use const instead of static
+const uint8_t speaker_mac[6] = {0x84,0xf3,0xeb,0xb2,0x32,0x9e};
+const uint8_t light1_mac[6] = {0x80,0x64,0x6f,0xb7,0xd5,0x93};
+const uint8_t light2_mac[6] = {0x80,0x64,0x6f,0xb6,0x86,0x3e};
 
-static uint8_t disco_mac[6] = {0xa4,0xe5,0x7c,0xbc,0xdb,0xe9};
-static uint8_t speaker_mac[6] = {0x84,0xf3,0xeb,0xb2,0x32,0x9e};
-static uint8_t light1_mac[6] = {0x80,0x64,0x6f,0xb7,0xd5,0x93};
-static uint8_t light2_mac[6] = {0x80,0x64,0x6f,0xb6,0x86,0x3e};
+void init_esp_now() {
+  if (esp_now_init() != 0) {  // Check for errors in ESP-NOW initialization
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+  esp_now_set_self_role(ESP_NOW_ROLE_CONTROLLER);
+}
+
+void add_peer(const uint8_t* mac) {
+  if (esp_now_add_peer(mac, ESP_NOW_ROLE_SLAVE, 1, NULL, 0) != 0) {  // Check for errors when adding peers
+    Serial.println("Failed to add peer");
+  }
+}
 
 void stop_party() {
-	uint8_t op = OP_STOP_DISCOTIME;
-	esp_now_send(disco_mac, &op, /* length: */ 1);
-	op = OP_PAUSE_MUSIC;
-	esp_now_send(speaker_mac, &op, /* length: */ 1);
-	op = OP_LIGHTS_ON;
-	esp_now_send(light1_mac, &op, /* length: */ 1);
-	esp_now_send(light2_mac, &op, /* length: */ 1);
+  uint8_t op = OP_STOP_DISCOTIME;
+  esp_now_send(disco_mac, &op, 1);
+  op = OP_PAUSE_MUSIC;
+  esp_now_send(speaker_mac, &op, 1);
+  op = OP_LIGHTS_ON;
+  esp_now_send(light1_mac, &op, 1);
+  esp_now_send(light2_mac, &op, 1);
 }
 
 void start_party() {
-	uint8_t op = OP_PLAY_MUSIC_WITH_DISCOTIME;
-	esp_now_send(speaker_mac, &op, /* length: */ 1);
-	op = OP_LIGHTS_OFF;
-	esp_now_send(light1_mac, &op, /* length: */ 1);
-	esp_now_send(light2_mac, &op, /* length: */ 1);
-
-	// trigger discotime to be sure it actually starts, in case speaker isn't operational:
-	delay(3000);
-	op = OP_DISCOTIME;
-	esp_now_send(disco_mac, &op, /* length: */ 1);
+  uint8_t op = OP_PLAY_MUSIC_WITH_DISCOTIME;
+  esp_now_send(speaker_mac, &op, 1);
+  op = OP_LIGHTS_OFF;
+  esp_now_send(light1_mac, &op, 1);
+  esp_now_send(light2_mac, &op, 1);
+  delay(3000);
+  op = OP_DISCOTIME;
+  esp_now_send(disco_mac, &op, 1);
 }
 
 void setup() {
-	esp_now_init();
-	esp_now_set_self_role(ESP_NOW_ROLE_CONTROLLER);
-	esp_now_add_peer(disco_mac, ESP_NOW_ROLE_SLAVE, 1, NULL, 0);
-	esp_now_add_peer(speaker_mac, ESP_NOW_ROLE_SLAVE, 1, NULL, 0);
-	esp_now_add_peer(light1_mac, ESP_NOW_ROLE_SLAVE, 1, NULL, 0);
-	esp_now_add_peer(light2_mac, ESP_NOW_ROLE_SLAVE, 1, NULL, 0);
+  Serial.begin(115200);  // Initialize Serial for debugging
+  
+  init_esp_now();  // Initialize ESP-NOW
+  
+  add_peer(disco_mac);
+  add_peer(speaker_mac);
+  add_peer(light1_mac);
+  add_peer(light2_mac);
 
-	pinMode(PARTY_STOP_PIN, INPUT);
-	if (digitalRead(PARTY_STOP_PIN)) {
-		stop_party();
-	} else {
-		start_party();
-	}
+  pinMode(PARTY_STOP_PIN, INPUT_PULLUP);  // Use INPUT_PULLUP mode for button
+  
+  // Debounce logic
+  delay(50);
+  if (digitalRead(PARTY_STOP_PIN)) {
+    stop_party();
+  } else {
+    start_party();
+  }
 
-	ESP.deepSleep(0, WAKE_RF_DISABLED);
+  ESP.deepSleep(0, WAKE_RF_DISABLED);
 }
 
 void loop() {
+  // Nothing here
 }
